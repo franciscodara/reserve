8a739249b3796216e18bb03515f373510fd70b59
Francisco Dara <franciscodara@gmail.com>
2021-01-06 18:22:28 -0300

    MongoDB incrementation with mongoose lib from MongoDB Atlas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://darauser:@dmin7865Dara@cluster0.hydcb.mongodb.net/reserve?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 app.use(express.json());
-app.use(routes);
+app.use(routes)
 
 app.listen(3333);
