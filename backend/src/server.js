968b8b329ead3a30d09db9909e18d65508b9f50a
Francisco Dara <franciscodara@gmail.com>
2021-01-06 18:42:22 -0300

    Mongoose UPDATE

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 mongoose.connect('mongodb+srv://darauser:@dmin7865Dara@cluster0.hydcb.mongodb.net/reserve?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 });
 
 app.use(express.json());
